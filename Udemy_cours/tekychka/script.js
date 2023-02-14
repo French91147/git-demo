7ddcebe1bddbfd7a850357a41c69ffef632b853a
French91147 <112475584+French91147@users.noreply.github.com>
2023-02-14 17:03:28 +0300

    lesson 019 callback-function (#12)

--- Udemy_cours/tekychka/script.js
+++ Udemy_cours/tekychka/script.js
@@ -1,88 +1,26 @@
-(function () { "use strict"; }());
+// (function () { "use strict"; }());
+"use strict"
 
-/* Практическое задание на урок 03: 
-
-1) Первую часть задания повторить по уроку
-
-2) Создать функцию showMyDB, которая будет проверять свойство privat. Если стоит в позиции
-false - выводит в консоль главный объект программы
-
-3) Создать функцию writeYourGenres в которой пользователь будет 3 раза отвечать на вопрос 
-"Ваш любимый жанр под номером ${номер по порядку}". Каждый ответ записывается в массив данных
-genres
-
-P.S. Функции вызывать не обязательно*/
-
-// Код возьмите из предыдущего домашнего задания
-
-
-let numberOfFilms;
-
-function start() {
-    numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = +prompt('Сколько фильмов вы уже посмотрели?', "");
-    }
+function first() {
+    // Do something
+    setTimeout(function () {
+        console.log(1);
+    }, 500);
 }
 
-start();
+function second() {
+    console.log(2);
+}
+first();
+second();
 
-const personalMovieDB = {  // не совсем понятно (что откуда берётся)
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    genres: [],
-    privat: false
-};
-
-function rememberMyFilms() {
-    for (let i = 0; i < 2; i++) {
-        const a = prompt('Один из последних просмотренных фильмов?', ""),
-            b = prompt('На сколько оцените его?', "");
-
-        if (a != null && b != null && a != "" && b != "" && a.length < 50) { // задача 02+выполнить ещё двумя способами. 
-            personalMovieDB.movies[a] = b;
-            console.log('done');
-        } else {
-            console.log("error");
-            i--;
-        }
-    }
+function learnJS(lang, callback) { // разобрать с Загадкой?
+    console.log(`Я учу: ${lang}`);
+    callback();
 }
 
-// rememberMyFilms();
-
-
-function detectPersonalLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("Просмотрено довольно мало фильмов"); // задача 03
-    } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
-        console.log("Вы классический зритель");
-    } else if (personalMovieDB.count >= 30) {
-        console.log("Вы киноман");
-    } else {
-        console.log("Произошла ошибка");
-    }
+function done() {
+    console.log("Я прошёл этот урок!");
 }
 
-// detectPersonalLevel();
-
-function showMyDB(hidden) { // задание 02:
-    if (!hidden) { // не до конца понятно преврщение false в true
-        console.log(personalMovieDB);
-    }
-}
-showMyDB(personalMovieDB.privat); //зачем тут писать аргумент, если в предыдущих вариантах их не писали?
-
-
-function writeYourGenres() {
-
-    for (let i = 1; i <= 3; i++) {
-        // const genre = prompt(`Ваш любимый жанр под номером ${i}`); можно писать так
-        // personalMovieDB.genres[i - 1] = genre;
-        personalMovieDB.genres[i - 1] = prompt(`Ваш любимый жанр под номером ${i}`); // или вот так
-    }
-}
-
-writeYourGenres();
+learnJS('JavaScript', done);
