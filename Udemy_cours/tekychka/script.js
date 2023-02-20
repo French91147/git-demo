c1171372fa7c7f411f9810aced109b60aae8edc7
Alexandr P <sanchobroda1974@gmail.com>
2023-02-20 21:45:59 +0300

    lesson 020 Objects, object destructuring

--- Udemy_cours/tekychka/script.js
+++ Udemy_cours/tekychka/script.js
@@ -1,26 +1,44 @@
-// (function () { "use strict"; }());
 "use strict"
 
-function first() {
-    // Do something
-    setTimeout(function () {
-        console.log(1);
-    }, 500);
-}
+const options = {
+    name: 'test',
+    width: 1024,
+    height: 1024,
+    colors: {
+        border: 'black',
+        bg: 'red'
+    },
+    makeTest: function () {  // метод созданный самостоятельно, а не встроенный в прогу, как // console.log(Object.keys(options).length); что ниже
+        console.log("Test");
+    }
+};
 
-function second() {
-    console.log(2);
-}
-first();
-second();
+options.makeTest();
 
-function learnJS(lang, callback) { // разобрать с Загадкой?
-    console.log(`Я учу: ${lang}`);
-    callback();
-}
+const { border, bg } = options.colors; //диструктуризация
 
-function done() {
-    console.log("Я прошёл этот урок!");
-}
+console.log(border);
 
-learnJS('JavaScript', done);
+// console.log(Object.keys(options).length); // подсчёт свойств в объекте, не через counter
+
+// console.log(options["colors"]["border"]); //пример объяснения работы for..in, что ниже
+
+// console.log(options.name);
+
+// delete options.name;
+
+// console.log(options);
+
+// let counter = 0;
+// for (let key in options) {  // " for..in  перебор свойств внутри объекта" пройти ещё раз, не всё очевидно.
+//     if (typeof (options[key]) === 'object') {
+//         for (let i in options[key]) {
+//             console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+//             counter ++;
+//         }
+//     } else {
+//         console.log(`Свойство ${key} имеет значение ${options[key]}`);
+//         counter ++;
+//     }
+// }
+// console.log(counter);
